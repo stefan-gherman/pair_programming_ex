76dcb30375ce3ecf24135101d36e6732687d888b
Stefan Gherman <stefangheman@gmail.com>
2019-11-07 19:23:57 +0200

    Finished Palindrome Function

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,8 +1,17 @@
-def palindrome(str):
-    return
+def palindrome(string):
+    original_str = string.lower().replace(' ', '')
+    #print(original_str)
+    reversed_str = original_str[-1::-1]
+    #print(reversed_str)
+    if original_str == reversed_str:
+        return True
+    else:
+        return False
 
 
 def main():
+    in1 = input("")
+    print(palindrome(in1))
     return
 
 
