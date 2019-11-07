7328554dbd85cd786b98ca5abb2a511142a9ddc5
Stefan Gherman <stefangheman@gmail.com>
2019-11-07 18:16:19 +0200

    finish FizzBuzz Exercise

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,8 +1,18 @@
 def fizzbuzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return number          
     return
 
 
 def main():
+    for i in range(1,101):
+        print(fizzbuzz(i))
     return
 
 if __name__ == '__main__':
