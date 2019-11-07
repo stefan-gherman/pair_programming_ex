3a6bbefb80d9792974c3d178978396879328c9c0
Stefan Gherman <stefangheman@gmail.com>
2019-11-08 00:17:43 +0200

    Answer for initial requirements

--- years/years_module.py
+++ years/years_module.py
@@ -2,10 +2,20 @@ import datetime
 
 
 def years(age):
+    year = datetime.date.today().year
+    if age < 100:
+        years_left = 100 - age
+        return years_left + year
+    else:
+        return year    
     return
 
 
 def main():
+    in1 = input("Name:")
+    in2 = input("Age:")
+    in2 = int(in2)
+    print(years(in2))
     return
 
 
