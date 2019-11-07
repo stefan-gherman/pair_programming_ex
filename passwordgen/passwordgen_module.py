16b2932f196eb63394a138cffda3171d01840df6
Stefan Gherman <stefangheman@gmail.com>
2019-11-08 01:47:32 +0200

    Creates a random strong password and a weak password

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,8 +1,25 @@
+import string
+import random as rd
+
 def passwordgen():
-    return
+   all_possible_chars = list(string.printable[:-6]) 
+
+   len_passw = 0
+   passw = ""
+
+   while len_passw <= 20:
+       passw += all_possible_chars[rd.randint(0,len(all_possible_chars)-1)]
+       len_passw += 1
+   return passw   
 
 
 def main():
+    in1 = input("Password Strenght w for weak, s for strong:")
+    if in1 == 'w':
+        weak_passwd = ['passwd', 'user', 'admin' , 'root']
+        print(weak_passwd[rd.randint(0,len(weak_passwd)-1)])
+    elif in1 == 's':
+        print(passwordgen())
     return
 
 
